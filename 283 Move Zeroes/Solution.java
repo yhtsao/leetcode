ebdc54fdc67372355752ab0192a4e1844d498e92
yhtsao <konve123.am99@gmail.com>
2018-06-09 15:04:20 +0800

    Optimize solution

--- 283 Move Zeroes/Solution.java
+++ 283 Move Zeroes/Solution.java
@@ -1,17 +1,17 @@
-import java.util.Arrays;
-
 class Solution {
     public void moveZeroes(int[] nums) {
+        int zero = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                for (int j = i; j > 0; j--) {
-                    if (nums[j - 1] != 0) break;
-                    int temp = nums[j];
-                    nums[j] = nums[j - 1];
-                    nums[j - 1] = temp;
-                }
+                swap(nums, zero, i);
+                zero++;
             }
         }
-        System.out.println(Arrays.toString(nums));
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
