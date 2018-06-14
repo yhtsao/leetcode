11de649de1aa2872da3139c4243c8dc77e60397f
yhtsao <konve123.am99@gmail.com>
2018-06-14 23:51:12 +0800

    Update 3 Sum Closest

---
+++ 16. 3Sum Closest/ThreeSumClosestTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ThreeSumClosestTest {
+    @Test
+    public void test01() {
+        int[] nums = new int[]{-1, 2, 1, -4};
+        int target = 1;
+
+        ThreeSumClosest threeSumClosest = new ThreeSumClosest();
+        int closest = threeSumClosest.threeSumClosest(nums, target);
+        Assertions.assertEquals(2, closest);
+    }
+}
