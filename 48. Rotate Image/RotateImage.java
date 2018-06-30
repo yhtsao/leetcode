78981bd509d6c037b481e7562dd26114bc517457
yhtsao <konve123.am99@gmail.com>
2018-06-30 16:21:19 +0800

    Update Rotate Image

--- 48. Rotate Image/RotateImage.java
+++ 48. Rotate Image/RotateImage.java
@@ -9,8 +9,7 @@ public class RotateImage {
 
         // swap matrix symmetrically
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                if (i >= j) continue;
+            for (int j = i + 1; j < matrix.length; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
