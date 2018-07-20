bfb35278cdcbc0830cc89a1b5d69177f9946393e
yhtsao <konve123.am99@gmail.com>
2018-07-20 23:23:41 +0800

    Update Valid Parentheses

---
+++ 20. Valid Parentheses/ValidParentheses.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (stack.isEmpty() || c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                char top = stack.peek();
+
+                if (top == '(' && c == ')')
+                    stack.pop();
+                else if (top == '[' && c == ']')
+                    stack.pop();
+                else if (top == '{' && c == '}')
+                    stack.pop();
+                else
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
