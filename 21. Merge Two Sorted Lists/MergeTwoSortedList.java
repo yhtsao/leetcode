5f0bd321aef2bf39d860769be26562a3776cca61
yhtsao <konve123.am99@gmail.com>
2018-07-24 08:20:59 +0800

    Update Merge Two Sorted List

---
+++ 21. Merge Two Sorted Lists/MergeTwoSortedList.java
@@ -0,0 +1,22 @@
+public class MergeTwoSortedList {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode curr = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = new ListNode(l1.val);
+                l1 = l1.next;
+            } else {
+                curr.next = new ListNode(l2.val);
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+
+        if (l1 != null)
+            curr.next = l1;
+        else if (l2 != null)
+            curr.next = l2;
+        return head.next;
+    }
+}
