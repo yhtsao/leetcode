018942a6cf11f0be7f73e3cf9776573c894c3aa4
yhtsao <konve123.am99@gmail.com>
2018-08-12 15:37:02 +0800

    Update Count and Say

---
+++ 38. Count and Say/CountAndSay.java
@@ -0,0 +1,23 @@
+public class CountAndSay {
+    public String countAndSay(int n) {
+        if (n < 1) return "";
+        String result = "1";
+        for (int i = 2; i <= n; i++) {
+            StringBuilder sb = new StringBuilder();
+            int count = 1;
+            char num = result.charAt(0);
+            for (int j = 1; j < result.length(); j++) {
+                if (result.charAt(j) == num)
+                    count++;
+                else {
+                    sb.append(count).append(num);
+                    count = 1;
+                    num = result.charAt(j);
+                }
+            }
+            sb.append(count).append(num);
+            result = sb.toString();
+        }
+        return result;
+    }
+}
