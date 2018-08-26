b80674171188081a82b8a8605aa9756d30156d61
yhtsao <konve123.am99@gmail.com>
2018-08-26 19:27:46 +0800

    Add N Queens

---
+++ 51. N-Queens/NQueensTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class NQueensTest {
+    private NQueens nQueens = new NQueens();
+
+    @Test
+    public void test01() {
+        int n = 4;
+        List<List<String>> results = nQueens.solveNQueens(n);
+
+        for (List<String> result : results) {
+            System.out.println("----------");
+            for (String row : result)
+                System.out.println(row);
+        }
+    }
+
+    @Test
+    public void test02() {
+        int n = 8;
+        List<List<String>> results = nQueens.solveNQueens(n);
+
+        for (List<String> result : results) {
+            System.out.println("----------");
+            for (String row : result)
+                System.out.println(row);
+        }
+    }
+}
+
