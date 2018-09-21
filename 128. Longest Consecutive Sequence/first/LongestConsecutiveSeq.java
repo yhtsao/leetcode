48b9b4377480426e6d50caff2ae51d7f80f5443f
yhtsao <konve123.am99@gmail.com>
2018-09-21 14:08:28 +0800

    Add Longest Consecutive Sequence

--- 128. Longest Consecutive Sequence/first/LongestConsecutiveSeq.java
+++ 128. Longest Consecutive Sequence/first/LongestConsecutiveSeq.java
@@ -1,7 +1,52 @@
 package first;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestConsecutiveSeq {
     public int longestConsecutive(int[] nums) {
-        return 0;
+        if (nums.length < 2) return nums.length;
+
+        return solutionByHashMap(nums);
+    }
+
+    private int solutionByHashMap(int[] nums) {
+        int max = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            // duplicate case
+            if (map.containsKey(nums[i]))
+                continue;
+
+            // find left and right boundary
+            int left = map.containsKey(nums[i] - 1) ? map.get(nums[i] - 1) : 0;
+            int right = map.containsKey(nums[i] + 1) ? map.get(nums[i] + 1) : 0;
+
+            // update new boundary
+            map.put(nums[i] - left, left + right + 1);
+            map.put(nums[i] + right, left + right + 1);
+            map.put(nums[i], left + right + 1);
+
+            max = Math.max(max, left + right + 1);
+        }
+        return max;
+    }
+
+    private int solutionBySorting(int[] nums) {
+        Arrays.sort(nums);
+        int max = 0;
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) continue;
+            if (nums[i] == nums[i - 1] + 1) {
+                count++;
+                continue;
+            }
+            max = Math.max(max, count);
+            count = 1;
+        }
+        max = Math.max(max, count);
+        return max;
     }
 }
--- 128. Longest Consecutive Sequence/first/LongestConsecutiveSeqTest.java
+++ 128. Longest Consecutive Sequence/first/LongestConsecutiveSeqTest.java
@@ -1,4 +1,64 @@
 package first;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class LongestConsecutiveSeqTest {
+    private LongestConsecutiveSeq longestConsecutiveSeq = new LongestConsecutiveSeq();
+
+    @Test
+    public void test01() {
+        int[] nums = new int[]{100, 4, 200, 1, 3, 2};
+        int ans = 4;
+        int result = longestConsecutiveSeq.longestConsecutive(nums);
+        Assertions.assertEquals(ans, result);
+    }
+
+    @Test
+    public void test02() {
+        int[] nums = new int[]{};
+        int ans = 0;
+        int result = longestConsecutiveSeq.longestConsecutive(nums);
+        Assertions.assertEquals(ans, result);
+    }
+
+    @Test
+    public void test03() {
+        int[] nums = new int[]{1};
+        int ans = 1;
+        int result = longestConsecutiveSeq.longestConsecutive(nums);
+        Assertions.assertEquals(ans, result);
+    }
+
+    @Test
+    public void test04() {
+        int[] nums = new int[]{2, 1};
+        int ans = 2;
+        int result = longestConsecutiveSeq.longestConsecutive(nums);
+        Assertions.assertEquals(ans, result);
+    }
+
+    @Test
+    public void test05() {
+        int[] nums = new int[]{1, 100};
+        int ans = 1;
+        int result = longestConsecutiveSeq.longestConsecutive(nums);
+        Assertions.assertEquals(ans, result);
+    }
+
+    @Test
+    public void test06() {
+        int[] nums = new int[]{1, 2, 0, 1};
+        int ans = 3;
+        int result = longestConsecutiveSeq.longestConsecutive(nums);
+        Assertions.assertEquals(ans, result);
+    }
+
+    @Test
+    public void test07() {
+        int[] nums = new int[]{4, 0, -4, -2, 2, 5, 2, 0, -8, -8, -8, -8, -1, 7, 4, 5, 5, -4, 6, 6, -3};
+        int ans = 5;
+        int result = longestConsecutiveSeq.longestConsecutive(nums);
+        Assertions.assertEquals(ans, result);
+    }
 }
