1dce2ae1a60c9ad9ade947357075c02b698a7b8a
yhtsao <konve123.am99@gmail.com>
2018-09-22 15:58:56 +0800

    Update Skyline problem

--- 218. The Skyline Problem/first/SkylineProblem.java
+++ 218. The Skyline Problem/first/SkylineProblem.java
@@ -34,7 +34,7 @@ public class SkylineProblem {
         }
     }
 
-    public List<int[]> getSkyline(int[][] buildings) {
+    public List<int[]> getSkyline1(int[][] buildings) {
         List<int[]> keyPoints = new ArrayList<>();
 
         List<Point> points = new ArrayList<>();
@@ -62,6 +62,44 @@ public class SkylineProblem {
 
             if (prevHeight != queue.lastKey()) {
                 keyPoints.add(new int[]{point.x, queue.lastKey()});
+            }
+        }
+
+        return keyPoints;
+    }
+
+    public List<int[]> getSkyline(int[][] buildings) {
+        List<int[]> keyPoints = new ArrayList<>();
+        List<int[]> points = new ArrayList<>();
+        for (int i = 0; i < buildings.length; i++) {
+            points.add(new int[]{buildings[i][0], -buildings[i][2]});
+            points.add(new int[]{buildings[i][1], buildings[i][2]});
+        }
+        Collections.sort(points, (a, b) -> {
+            if (a[0] == b[0]) {
+                return a[1] - b[1];
+            }
+            return a[0] - b[0];
+        });
+
+        TreeMap<Integer, Integer> queue = new TreeMap<>();
+        queue.put(0, 1);
+        for (int[] point : points) {
+            int height = point[1];
+            int prevHeight = queue.lastKey();
+            if (height < 0)
+                queue.compute(-height, (key, value) -> {
+                    if (value != null) return value + 1;
+                    return 1;
+                });
+            else
+                queue.compute(height, (key, value) -> {
+                    if (value == 1) return null;
+                    return value - 1;
+                });
+
+            if (prevHeight != queue.lastKey()) {
+                keyPoints.add(new int[]{point[0], queue.lastKey()});
             }
         }
 
