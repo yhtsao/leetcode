3c13228a0096ad6bfe0dc56c645ba4a9d1ece134
yhtsao <konve123.am99@gmail.com>
2018-09-28 17:38:43 +0800

    Add DP solution for Interleaving String

--- 97. Interleaving String/first/InterleavingString.java
+++ 97. Interleaving String/first/InterleavingString.java
@@ -3,10 +3,30 @@ package first;
 public class InterleavingString {
     public boolean isInterleave(String s1, String s2, String s3) {
         if (s1.length() + s2.length() != s3.length()) return false;
-        return isInterleaveRecur(s1, s2, s3);
+        return isInterleaveDP(s1, s2, s3);
     }
 
-    public boolean isInterleaveRecur(String s1, String s2, String s3) {
+    private boolean isInterleaveDP(String s1, String s2, String s3) {
+        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= s1.length(); i++) {
+            dp[i][0] = s1.charAt(i - 1) == s3.charAt(i - 1) && dp[i - 1][0];
+        }
+        for (int i = 1; i <= s2.length(); i++) {
+            dp[0][i] = s2.charAt(i - 1) == s3.charAt(i - 1) && dp[0][i - 1];
+        }
+
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                int k = i + j - 1;
+                dp[i][j] = (s1.charAt(i - 1) == s3.charAt(k) && dp[i - 1][j])
+                        || (s2.charAt(j - 1) == s3.charAt(k) && dp[i][j - 1]);
+            }
+        }
+        return dp[s1.length()][s2.length()];
+    }
+
+    private boolean isInterleaveRecur(String s1, String s2, String s3) {
         if (s1.length() + s2.length() != s3.length()) return false;
         int[][] memo = new int[s1.length()][s2.length()];
         return recur(s1, 0, s2, 0, s3, 0, memo);
