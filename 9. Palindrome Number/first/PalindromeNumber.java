3f899455ff43cb32817043bfa10d5c3efeeb8247
yhtsao <konve123.am99@gmail.com>
2018-09-28 09:55:17 +0800

    Update solution for Palindrome Number

--- 9. Palindrome Number/first/PalindromeNumber.java
+++ 9. Palindrome Number/first/PalindromeNumber.java
@@ -2,9 +2,18 @@ package first;
 
 public class PalindromeNumber {
     public boolean isPalindrome(int x) {
-        if (x < 0) return false;
+        if (x < 0 || (x > 0 && x % 10 == 0)) return false;
 
-        return isPalindromeByStr(x);
+        return isPalindromeWoStr(x);
+    }
+
+    private boolean isPalindromeWoStr(int x) {
+        int res = 0;
+        while (x > res) {
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+        return x == res || x == res / 10;
     }
 
     private boolean isPalindromeByStr(int x) {
