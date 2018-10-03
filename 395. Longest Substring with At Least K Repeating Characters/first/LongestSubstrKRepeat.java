26b091a373b55216c49a5b63529aee2eb66481a5
yhtsao <konve123.am99@gmail.com>
2018-10-03 14:26:25 +0800

    Add two pointers solution for Longest Substring with At Least K Repeating Characters

--- 395. Longest Substring with At Least K Repeating Characters/first/LongestSubstrKRepeat.java
+++ 395. Longest Substring with At Least K Repeating Characters/first/LongestSubstrKRepeat.java
@@ -3,6 +3,10 @@ package first;
 public class LongestSubstrKRepeat {
     public int longestSubstring(String s, int k) {
         if (k < 2) return s.length();
+        return longestSubstringTwoPtr(s, k);
+    }
+
+    private int longestSubstringDivide(String s, int k) {
         return recur(s, 0, s.length(), k);
     }
 
@@ -25,5 +29,41 @@ public class LongestSubstrKRepeat {
             }
         }
         return hi - lo;
+    }
+
+    private int longestSubstringTwoPtr(String s, int k) {
+        int maxLen = 0;
+        for (int i = 1; i <= 26; i++) {
+            maxLen = Math.max(maxLen, longestSubstringWithNUniqueChar(s, k, i));
+        }
+        return maxLen;
+    }
+
+    /**
+     * Find longest substring contains n unique characters
+     * and the occurrence of each character is at least k times
+     */
+    private int longestSubstringWithNUniqueChar(String s, int k, int unique) {
+        int start = 0, end = 0; // two pointers
+        int[] map = new int[26];
+        int maxLen = 0;
+        int numNoLessThanK = 0; // number of characters occurs at least k times
+        int currUnique = 0; // number of unique characters
+
+        for (end = 0; end < s.length(); end++) {
+            if (map[s.charAt(end) - 'a']++ == 0) currUnique++;
+            if (map[s.charAt(end) - 'a'] == k) numNoLessThanK++;
+
+            while (currUnique > unique) {
+                if (map[s.charAt(start) - 'a']-- == k) numNoLessThanK--;
+                if (map[s.charAt(start++) - 'a'] == 0) currUnique--;
+            }
+
+            if (currUnique == unique && numNoLessThanK == unique) {
+                maxLen = Math.max(maxLen, end - start + 1);
+            }
+        }
+
+        return maxLen;
     }
 }
