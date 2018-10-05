f4874ad6895c7a5b6a943fb3143cfc0daaf843d3
yhtsao <konve123.am99@gmail.com>
2018-10-05 11:32:35 +0800

    Add Binary Tree Level Order Traversal (1 Queue solution)

--- 102. Binary Tree Level Order Traversal/first/BinaryTreeLevelOrder.java
+++ 102. Binary Tree Level Order Traversal/first/BinaryTreeLevelOrder.java
@@ -9,6 +9,25 @@ public class BinaryTreeLevelOrder {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
         if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int queueSize = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < queueSize; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            res.add(list);
+        }
+
+        return res;
+    }
+
+    public List<List<Integer>> levelOrderWith2Queue(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
         Queue<TreeNode> oddLevel = new LinkedList<>();
         Queue<TreeNode> evenLevel = new LinkedList<>();
         oddLevel.offer(root);
