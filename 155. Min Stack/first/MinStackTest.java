867d4b4732e8a15e6510a9297b44885a4aefbb2d
yhtsao <konve123.am99@gmail.com>
2018-10-12 11:24:56 +0800

    Add Min Stack

---
+++ 155. Min Stack/first/MinStackTest.java
@@ -0,0 +1,19 @@
+package first;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MinStackTest {
+
+    @Test
+    public void test01() {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        Assertions.assertEquals(-3, minStack.getMin());
+        minStack.pop();
+        Assertions.assertEquals(0, minStack.top());
+        Assertions.assertEquals(-2, minStack.getMin());
+    }
+}
