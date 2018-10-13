bf55a5ec3615df689e10245e6050683081dbf5a3
yhtsao <konve123.am99@gmail.com>
2018-10-13 14:48:08 +0800

    Add Binary Search tree and Binary index tree solution for "Count of Smaller Numbers After Self"

--- 315. Count of Smaller Numbers After Self/first/SmallerCountAfterSelf.java
+++ 315. Count of Smaller Numbers After Self/first/SmallerCountAfterSelf.java
@@ -7,6 +7,13 @@ import java.util.List;
 public class SmallerCountAfterSelf {
 
     public List<Integer> countSmaller(int[] nums) {
+        return countSmallerBinaryIndexedTree(nums);
+    }
+
+    /* ---------------------------------------------------------------
+     * Merge Sort solution
+     * ---------------------------------------------------------------*/
+    public List<Integer> countSmallerMergeSort(int[] nums) {
         List<Integer> res = new ArrayList<>();
         int[][] pairs = new int[nums.length][2];
         for (int i = 0; i < nums.length; i++) {
@@ -36,4 +43,99 @@ public class SmallerCountAfterSelf {
         }
         return nums;
     }
+
+    /* ---------------------------------------------------------------
+     * Binary search tree solution
+     * ---------------------------------------------------------------*/
+    class TreeNode {
+        TreeNode left;
+        TreeNode right;
+        int val;
+        int index;
+        int lsize;
+
+        TreeNode(int index, int val) {
+            this.index = index;
+            this.val = val;
+            this.lsize = 0;
+        }
+    }
+
+    public List<Integer> countSmallerBinarySearchTree(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        if (nums.length == 0) return res;
+        int[] count = new int[nums.length];
+        TreeNode root = new TreeNode(nums.length - 1, nums[nums.length - 1]);
+        TreeNode cur;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            cur = root;
+            while (cur != null) {
+                if (nums[i] <= cur.val) {
+                    cur.lsize++;
+                    if (cur.left == null) {
+                        cur.left = new TreeNode(i, nums[i]);
+                        break;
+                    }
+                    cur = cur.left;
+                } else {
+                    count[i] += cur.lsize + 1;
+                    if (cur.right == null) {
+                        cur.right = new TreeNode(i, nums[i]);
+                        break;
+                    }
+                    cur = cur.right;
+                }
+            }
+        }
+
+        for (int c : count) res.add(c);
+        return res;
+    }
+
+    /* ---------------------------------------------------------------
+     * Binary indexed tree solution (Fenwick Tree)
+     * ---------------------------------------------------------------*/
+    public List<Integer> countSmallerBinaryIndexedTree(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        if (nums.length == 0) return res;
+
+        // Find minimum value of nums
+        // Ex: nums = [5, 3, 6, -2, 1]
+        // min = -2
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++)
+            min = Math.min(min, nums[i]);
+
+        // Shift array to make sure every element in array is larger than 0
+        // nums = [8, 6, 9, 1, 2]
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] - min + 1;
+            max = Math.max(max, nums[i]);
+        }
+
+        // Update Binary index tree and count the smaller number
+        int[] tree = new int[max + 1];
+        for (int i = nums.length - 1; i >= 0; i--) {
+            res.add(0, getSum(tree, nums[i] - 1));
+            update(tree, nums[i], 1);
+        }
+        return res;
+    }
+
+    private void update(int[] tree, int index, int val) {
+        while (index < tree.length) {
+            tree[index] += val;
+            index += index & (-index);
+        }
+    }
+
+    private int getSum(int[] tree, int index) {
+        int sum = 0;
+        while (index > 0) {
+            sum += tree[index];
+            index -= index & (-index);
+        }
+        return sum;
+    }
+
 }
