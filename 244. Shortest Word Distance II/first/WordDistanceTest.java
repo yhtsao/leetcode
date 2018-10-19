257866c87c5777c0adcc0ef5dc118edd4d554626
yhtsao <konve123.am99@gmail.com>
2018-10-19 12:08:05 +0800

    Add Shortest word distance II

---
+++ 244. Shortest Word Distance II/first/WordDistanceTest.java
@@ -0,0 +1,30 @@
+package first;
+
+import first.sol1.WordDistance;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class WordDistanceTest {
+
+    @Test
+    public void test01() {
+        WordDistance wordDistance = new WordDistance(
+                new String[]{"practice", "makes", "perfect", "coding", "makes"});
+        Assertions.assertEquals(3, wordDistance.shortest("practice", "coding"));
+        Assertions.assertEquals(1, wordDistance.shortest("makes", "coding"));
+    }
+
+    @Test
+    public void test02() {
+        String[] words = new String[]{"x", "a", "x", "x", "x", "b", "x", "x", "b", "a"};
+        WordDistance wordDistance = new WordDistance(words);
+        Assertions.assertEquals(1, wordDistance.shortest("a", "b"));
+    }
+
+    @Test
+    public void test03() {
+        String[] words = new String[]{"a", "x", "x", "x", "b", "x", "a", "x", "x", "x", "x", "b"};
+        WordDistance wordDistance = new WordDistance(words);
+        Assertions.assertEquals(2, wordDistance.shortest("a", "b"));
+    }
+}
---
+++ 244. Shortest Word Distance II/first/sol1/WordDistance.java
@@ -0,0 +1,31 @@
+package first.sol1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WordDistance {
+    Map<String, List<Integer>> map;
+
+    public WordDistance(String[] words) {
+        this.map = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+            List<Integer> list = map.computeIfAbsent(words[i], value -> new ArrayList<>());
+            list.add(i);
+        }
+    }
+
+    public int shortest(String word1, String word2) {
+        List<Integer> indexOfWord1 = map.get(word1);
+        List<Integer> indexOfWord2 = map.get(word2);
+        int min = Integer.MAX_VALUE;
+        int i = 0, j = 0;
+        while (i < indexOfWord1.size() && j < indexOfWord2.size()) {
+            min = Math.min(min, Math.abs(indexOfWord1.get(i) - indexOfWord2.get(j)));
+            if (indexOfWord1.get(i) < indexOfWord2.get(j)) i++;
+            else j++;
+        }
+        return min;
+    }
+}
