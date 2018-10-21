148537a06a3807f735f3c80ea90f18ee43f59bc4
yhtsao <konve123.am99@gmail.com>
2018-10-21 08:51:14 +0800

    Add Bit Manipulation solution for Subsets

--- 78. Subsets/first/Subsets.java
+++ 78. Subsets/first/Subsets.java
@@ -5,11 +5,16 @@ import java.util.List;
 
 public class Subsets {
     public List<List<Integer>> subsets(int[] nums) {
+        return subsetsBit(nums);
+    }
+
+    public List<List<Integer>> subsetsBacktracking(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         result.add(new ArrayList<>());
         backtracking(result, new ArrayList<>(), nums, 0);
         return result;
     }
+
 
     private void backtracking(List<List<Integer>> result, List<Integer> list, int[] nums, int start) {
         if (start == nums.length) {
@@ -22,5 +27,18 @@ public class Subsets {
             backtracking(result, list, nums, i + 1);
             list.remove(list.size() - 1);
         }
+    }
+
+    private List<List<Integer>> subsetsBit(int[] nums) {
+        List<List<Integer>> ret = new ArrayList<>();
+        int total = 1 << nums.length;
+        for (int i = 0; i < total; i++) {
+            List<Integer> list = new ArrayList<>();
+            for (int j = 0; j < nums.length; j++) {
+                if (((i >> j) & 1) == 1) list.add(nums[j]);
+            }
+            ret.add(list);
+        }
+        return ret;
     }
 }
