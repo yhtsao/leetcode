104be9b85114d14eec2884fd27429a1ea7270ca3
yhtsao <konve123.am99@gmail.com>
2018-10-24 15:48:08 +0800

    Add Minimize Malware Spread II

---
+++ 913. Cat and Mouse/first/CatAndMouse.java
@@ -0,0 +1,7 @@
+package first;
+
+public class CatAndMouse {
+    public int catMouseGame(int[][] graph) {
+        return 0;
+    }
+}
---
+++ 913. Cat and Mouse/first/CatAndMouseTest.java
@@ -0,0 +1,20 @@
+package first;
+
+import org.junit.jupiter.api.Test;
+
+public class CatAndMouseTest {
+    private CatAndMouse catAndMouse = new CatAndMouse();
+
+    @Test
+    public void test01() {
+        int[][] graph = new int[][]{
+                {2, 5},
+                {3},
+                {0, 4, 5},
+                {1, 4, 5},
+                {2, 3},
+                {0, 2, 3}
+        };
+        int ans = 0;
+    }
+}
