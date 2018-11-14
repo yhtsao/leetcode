2a81b94bd89e4c73f00108c15df11b2d708a27fa
yhtsao <konve123.am99@gmail.com>
2018-11-14 09:44:57 +0800

    Update Missing Ranges

--- 163. Missing Ranges/first/MissingRanges.java
+++ 163. Missing Ranges/first/MissingRanges.java
@@ -35,4 +35,50 @@ public class MissingRanges {
 
         return ret;
     }
+
+    public List<String> findMissingRangesSol2(int[] nums, int lower, int upper) {
+        List<String> ranges = new ArrayList<>();
+        long start = (long) lower, end;
+        for (int num : nums) {
+            if (num < start + 1) {
+                start = (long) num + 1;
+                continue;
+            }
+            end = (long) Math.min(upper, num - 1);
+            ranges.add(intervalStr(start, end));
+            start = (long) num + 1;
+        }
+        if (start <= upper)
+            ranges.add(intervalStr(start, upper));
+        return ranges;
+    }
+
+    public List<String> findMissingRangesSol3(int[] nums, int lower, int upper) {
+        List<String> ranges = new ArrayList<>();
+        if (nums.length == 0) {
+            ranges.add(intervalStr(lower, upper));
+            return ranges;
+        }
+        if (lower == Integer.MAX_VALUE) return ranges;
+
+        int start = lower, end;
+        for (int num : nums) {
+            if (num < start + 1) {
+                start = num + 1;
+                continue;
+            }
+            end = Math.min(upper, num - 1);
+            ranges.add(intervalStr(start, end));
+            if (num == upper) return ranges;
+            start = num + 1;
+        }
+        if (start <= upper)
+            ranges.add(intervalStr(start, upper));
+        return ranges;
+    }
+
+    private String intervalStr(long lo, long hi) {
+        if (lo == hi) return "" + lo;
+        else return lo + "->" + hi;
+    }
 }
